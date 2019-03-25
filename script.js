0e0cd5680569aaca0233500f8e975f426763fb33
CakeMuffin <46337383+CakeMuffin@users.noreply.github.com>
2019-03-25 20:46:53 +0600

    a new site has been born
    Behold!!!!

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function (){
+var n = 0;
+var butt = document.getElementById("start");
+var c = 300000;
+
+butt.onclick = function (){
+  v = document.getElementById("YourSpeed").value;
+  T1 = 1 / Math.sqrt(1 - (Math.pow(v, 2)/Math.pow(c, 2))); 
+  document.getElementById('Output').value=T1;
+};
+}
