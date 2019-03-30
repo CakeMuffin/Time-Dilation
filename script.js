e3196e801aa703b9c665aa56b883836902699438
CakeMuffin <46337383+CakeMuffin@users.noreply.github.com>
2019-03-30 23:42:11 +0600

    Dropdown

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ butt.onclick = function (){
   v = document.getElementById("YourSpeed").value;
   T1 = 1 / Math.sqrt(1 - (Math.pow(v, 2)/Math.pow(c, 2))); 
   document.getElementById('Output').value=T1;
+  document.getElementById('anim').className ='slide-right'
 };
 }
